4500526df8e3fd9f2cf0c7c827d690c6c1514495
Tomas Jacik <tomas@jacik.cz>
2021-02-17 00:24:33 +0100

    PHPStan: fixed error for PHP 7.3

--- src/Localization/ReturnTranslator.php
+++ src/Localization/ReturnTranslator.php
@@ -9,6 +9,10 @@ use Nette\Localization\ITranslator;
 final class ReturnTranslator implements ITranslator
 {
 
+	/**
+	 * @param mixed $message
+	 * @param mixed ...$parameters
+	 */
 	public function translate($message, ...$parameters): string
 	{
 		return $message;
