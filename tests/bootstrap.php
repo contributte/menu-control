19bd05f62ec137a148ce7590938322d40cbf340e
Tomas Jacik <tomas@jacik.cz>
2021-02-17 14:10:48 +0100

    Tests: fixed errors when container is generated during PHPCS run

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,5 +8,9 @@ Tester\Environment::setup();
 Tester\Environment::bypassFinals();
 date_default_timezone_set('Europe/Prague');
 
-define('TEMP_DIR', __DIR__. '/tmp/'. getmypid());
+if (!is_dir(__DIR__ . '/../tmp')) {
+	mkdir(__DIR__ . '/../tmp/');
+}
+
+define('TEMP_DIR', __DIR__ . '/../tmp/'. getmypid());
 Tester\Helpers::purge(TEMP_DIR);
